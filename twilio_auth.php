546f4b74740f5796bcf85e772a4b9ce37d741ee5
Doug Cone <doug@nullvariable.com>
2011-08-20 15:19:57 -0400

    added options

--- twilio_auth.php
+++ twilio_auth.php
@@ -7,12 +7,25 @@ Author: Nullvariable Web Consulting
 Author URI: http://www.nullvariable.com
 */
 
+/**
+ * Queue our WordPress hooks
+ */
+
+if ( is_admin() ){ // admin actions
+  add_action('admin_menu', 'twilio_auth_opts_page');
+  add_action( 'admin_init', 'twilio_auth_register_settings' );
+} else {
+  // non-admin enqueues, actions, and filters
+}
+
+function twilio_auth_register_settings() { // whitelist options
+  register_setting( 'twilio-auth', 'twilio_auth_AccountSid' );
+  register_setting( 'twilio-authp', 'twilio_auth_AuthToken' );
+}
 
 /**
  * Add our config options page to WordPress
  */
-add_action('admin_menu', 'twilio_auth_opts_page');
-
 function twilio_auth_opts_page() {
   add_options_page('Twilio Two Factor','Twilio Two Factor', 'update_core', 'twilio-auth', 'twilio_auth_opts_page_render');
 }
@@ -20,5 +33,14 @@ function twilio_auth_opts_page() {
 function twilio_auth_opts_page_render() {
 	print '<div class="wrap">';
 	print "<h2>" . __( 'Twilio Auth Settings', 'twilio-auth-settings-page' ) . "</h2>";
+	print '<form method="post" action="options.php">';
+	settings_fields( 'twilio-auth' );
+	do_settings_fields( 'twilio-auth' ); ?>
+	<p class="submit">
+		<input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />
+	</p>
+	</form><?php
 	print '</div>';
 }
+
+
