f1870a27b83c21bb7e91f191bf9b1d4d94a1010b
Doug Cone <doug@nullvariable.com>
2011-08-20 15:07:07 -0400

    options/settings page build

--- twilio_auth.php
+++ twilio_auth.php
@@ -14,9 +14,11 @@ Author URI: http://www.nullvariable.com
 add_action('admin_menu', 'twilio_auth_opts_page');
 
 function twilio_auth_opts_page() {
-  add_options_page('Twilio Two Factor','Twilio Two Factor', 'manage_sites', 'twilio-auth', 'twilio_auth_opts_page_render');
+  add_options_page('Twilio Two Factor','Twilio Two Factor', 'update_core', 'twilio-auth', 'twilio_auth_opts_page_render');
 }
 
 function twilio_auth_opts_page_render() {
-	
+	print '<div class="wrap">';
+	print "<h2>" . __( 'Twilio Auth Settings', 'twilio-auth-settings-page' ) . "</h2>";
+	print '</div>';
 }
