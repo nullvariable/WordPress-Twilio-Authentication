67d39aeb28a420f030c3a579e0543f3731000576
Doug Cone <doug@nullvariable.com>
2011-08-20 14:35:32 -0400

    initial commit

---
+++ twilio_auth.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Two Factor Authentication with Twilio
+Description: API implmentaion allowing two-factor authentication via Twilio
+Version: 0.1
+Author: Nullvariable Web Consulting
+Author URI: http://www.nullvariable.com
+*/
+
+
